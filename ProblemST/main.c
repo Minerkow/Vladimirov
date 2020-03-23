ffe8a02585d80a3fd893039f209519b0139cc937
Minerkow <54900345+Minerkow@users.noreply.github.com>
2020-03-23 03:02:13 +0300

    Create main.c

---
+++ ProblemST/main.c
@@ -0,0 +1,28 @@
+#include "HeaderLX.h"
+
+enum { MAXLEN = 1024 };
+
+
+int main() {
+    //FILE* f = fopen("/home/bibi/CLionProject/Vladimirov/ProblemST_New/tests/001.expr", "r");
+    int res = 0;
+    struct lex_array_t larr = {};
+    char inp[MAXLEN] = {0};
+
+    res = scanf("%1023c", inp);
+    assert(res == 1);
+
+    larr = lex_string(inp);
+
+    if (larr.lexems == NULL) {
+        printf("ERROR\n");
+        return 0;
+    }
+
+    dump_lexarray(larr);
+    printf("\n");
+    struct node_t* top = BuildTree(larr);
+    free(larr.lexems);
+    free(top);
+    return 0;
+}
