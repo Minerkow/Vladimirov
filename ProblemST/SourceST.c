0e4118ed70de1afe9a5f7ac1e588a09efdf9cd08
Minerkow <54900345+Minerkow@users.noreply.github.com>
2020-04-04 16:43:27 +0300

    Update SourceST.c

--- ProblemST/SourceST.c
+++ ProblemST/SourceST.c
@@ -1,14 +1,61 @@
-
 //
-// Created by Minerkow on 14.03.2020.
+// Created by bibi on 18.03.2020.
 //
 
 #include "HeaderLX.h"
 
+static struct lexem_t get_cur_lexem (int i, struct lex_array_t* ptr);
+static int get_cur_size(struct lex_array_t* ptr);
+static int is_add_sub (int i);
+static int is_mul_div(int i);
+static int is_brace(int i);
+
+static struct lexem_t get_cur_lexem (int i, struct lex_array_t* ptr)
+{
+    static struct lex_array_t* larr = NULL;
+    if (ptr != NULL)
+        larr = ptr;
+    return (larr->lexems[i]);
+}
+
+static int get_cur_size(struct lex_array_t* ptr)
+{
+    static struct lex_array_t* larr = NULL;
+    if (ptr != NULL)
+        larr = ptr;
+    return (larr->size);
+}
+
+static int is_add_sub (int i)
+{
+    return (get_cur_lexem(i, NULL).lex.op == ADD || get_cur_lexem(i, NULL).lex.op == SUB);
+}
+
+static int is_mul_div(int i)
+{
+    return (get_cur_lexem(i, NULL).lex.op == MUL || get_cur_lexem(i, NULL).lex.op == DIV);
+}
+
+static int is_brace(int i)
+{
+    if (get_cur_lexem(i, NULL).lex.b == LBRAC && get_cur_lexem(i, NULL).kind == BRACE)
+        return LBRAC;
+    if (get_cur_lexem(i, NULL).lex.b == RBRAC && get_cur_lexem(i, NULL).kind == BRACE)
+        return RBRAC;
+    return 0;
+}
+
 struct node_t* BuildTree (struct lex_array_t larr)
 {
     int i = 0;
-    struct node_t* top = Expr(&larr, &i);
+    get_cur_lexem(0, &larr);
+    get_cur_size(&larr);
+    struct node_t* top = Expr(&i);
+    if (top == NULL)
+    {
+        printf ("Empty input");
+        exit(1);
+    }
     printf("\n[%d]\n", calc_result(top));
     return top;
 }
@@ -34,41 +81,66 @@ int calc (int l, int r, struct node_t *top)
         return l / r;
 }
 
-struct node_t* Expr(struct lex_array_t* larr, int* i)
+struct node_t* Expr(int* i)
 {
     struct node_t* node = NULL;
-    struct node_t* expr_left = Mult(larr, i);
-    if (*i == larr->size-1)
+    struct node_t* expr_left = Mult(i);
+    if (*i == get_cur_size(NULL) - 1)
         return expr_left;
     (*i)++;
-    if (larr->lexems[*i].kind == OP) {
-        if (larr->lexems[*i].lex.op == ADD || larr->lexems[*i].lex.op == SUB) {
+
+    if (is_brace(*i) == RBRAC)
+    {
+        printf("Extra brace");
+        exit(3);
+    }
+
+    if (get_cur_lexem(*i, NULL).kind == OP) {
+        if (is_add_sub(*i)) {
             node = Create_Node();
-            node->lexem = larr->lexems[*i];
+            node->lexem = get_cur_lexem(*i, NULL);
             (*i)++;
+            struct lexem_t lexem = get_cur_lexem(*i, NULL);
+            if (lexem.kind != NUM && lexem.kind != BRACE)
+            {
+                printf("Expected expression");
+                exit(5);
+            }
             node->left = expr_left;
-            node->right = Expr(larr, i);
+            node->right = Expr(i);
             return node;
         }
         (*i)--;
     }
+
     return expr_left;
 }
 
-struct node_t* Mult(struct lex_array_t* larr, int* i)
+struct node_t* Mult(int* i)
 {
     struct node_t* node = NULL;
-    struct node_t* mult_left = Term(larr, i);
-    if (*i == larr->size-1)
+    struct node_t* mult_left = Term(i);
+    if (*i == get_cur_size(NULL) - 1)
         return mult_left;
     (*i)++;
-    if (larr->lexems[*i].kind == OP) {
-        if (larr->lexems[*i].lex.op == MUL || larr->lexems[*i].lex.op == DIV) {
+    if (get_cur_lexem(*i, NULL).kind == NUM)
+    {
+        printf("Two numbers in a row");
+        exit(4);
+    }
+    if (get_cur_lexem(*i, NULL).kind == OP) {
+        if (is_mul_div(*i)) {
             node = Create_Node();
-            node->lexem = larr->lexems[*i];
+            node->lexem = get_cur_lexem(*i, NULL);
             (*i)++;
+            struct lexem_t lexem = get_cur_lexem(*i, NULL);
+            if (lexem.kind != NUM && lexem.kind != BRACE)
+            {
+                printf("Expected expression");
+                exit(5);
+            }
             node->left = mult_left;
-            node->right = Mult(larr, i);
+            node->right = Mult(i);
             return node;
         }
         (*i)--;
@@ -76,24 +148,31 @@ struct node_t* Mult(struct lex_array_t* larr, int* i)
     return mult_left;
 }
 
-struct node_t* Term(struct lex_array_t* larr, int* i)
+struct node_t* Term (int* i)
 {
     struct node_t* node = NULL;
-    if (larr->lexems[*i].kind == NUM)
+    if (get_cur_lexem(*i, NULL).kind == NUM)
     {
         node = Create_Node();
-        node->lexem = larr->lexems[*i];
+        node->lexem = get_cur_lexem(*i, NULL);
         /*if (larr->size - 1 != (*i))
             (*i)++;*/
         return node;
     }
-    if(larr->lexems[*i].lex.b == LBRAC && larr->lexems[*i].kind == BRACE)
+    if(is_brace(*i) == LBRAC)
     {
         (*i)++;
-        node = Expr(larr, i);
+        struct lexem_t lexem = get_cur_lexem(*i, NULL);
+        if (lexem.kind == OP)
+        {
+            printf("Unexpected operation");
+            exit(5);
+        }
+        node = Expr(i);
         return node;
     }
-    if (larr->lexems[*i].lex.b == RBRAC && larr->lexems[*i].kind == BRACE)
+
+    if (is_brace(*i) == RBRAC)
     {
         (*i)++;
         return node;
@@ -104,6 +183,9 @@ struct node_t* Term(struct lex_array_t* larr, int* i)
 struct node_t* Create_Node()
 {
     struct node_t* node = (struct node_t*)calloc(1, sizeof(struct node_t));
+
+    assert(node);
+
     return node;
 }
 
@@ -113,4 +195,66 @@ void free_tree(struct node_t *t) {
     free_tree(t->right);
     free(t);
 }
+
+
+
+
+
+
+
+
+/*
+void print_node (struct lexem_t lex) {
+    switch (lex.kind) {
+        case OP:
+            switch (lex.lex.op) {
+                case ADD:
+                    printf ("ADD ");
+                    break;
+                case SUB:
+                    printf ("SUB ");
+                    break;
+                case MUL:
+                    printf ("MUL ");
+                    break;
+                case DIV:
+                    printf ("DIV ");
+                    break;
+                default:
+                    exit(1);
+            }
+            break;
+        case NUM:
+            printf ("%d ", lex.lex.num);
+            break;
+        default:
+            exit(1);
+    }
+}
+
+void print_tree (struct node_t* top) {
+    if (top == NULL){
+        printf ("Error: top is NULL\n");
+        return;
+    }
+    if (top->left == NULL && top->right == NULL)
+        return;
+
+    print_node (top->lexem);
+
+    if (top->left == NULL) {
+        printf ("\n");
+        return;
+    }
+    print_node (top->left->lexem);
+
+    if (top->right == NULL)
+        return;
+    print_node (top->right->lexem);
+
+    printf ("\n");
+
+    print_tree (top->left);
+    print_tree (top->right);
+}*/
 
